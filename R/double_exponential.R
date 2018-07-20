fccd3f4d17ec848969f971254ae3dce45e98f21b
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-07-20 08:20:25 +0200

    a few more plotting updates

--- R/double_exponential.R
+++ R/double_exponential.R
@@ -1,4 +1,5 @@
 library(cowplot)
+library(tidyverse)
 
 
 
@@ -9,21 +10,47 @@ B2 <- 0.18
 D0 <- 1
 D1 <- 0.128
 D2 <- 0.32
-x <- seq(0, 30, by =0.1)
+
+B1 <- data %>% 
+	filter(Species == "CS", term == "b1") %>% 
+	select(estimate)
+B2 <- data %>% 
+	filter(Species == "CS", term == "b2") %>% 
+	select(estimate)
+D0 <- data %>% 
+	filter(Species == "CS", term == "d0") %>% 
+	select(estimate)
+D1 <- data %>% 
+	filter(Species == "CS", term == "d1") %>% 
+	select(estimate)
+D2 <- data %>% 
+	filter(Species == "CS", term == "d2") %>% 
+	select(estimate)
+
+
+B1 <- 0.08
+B2 <- 0.09
+D0 <- 0
+D1 <- 5.952219e-06
+D2 <- 0.39
+
+x <- seq(0, 40, by =0.1)
+
 mod_pred <- function(x) {
-	y <- (B1*exp(B2*x) - (D0 + (D1*exp(D2*x))))
+	y <- (B1[[1]]*exp(B2[[1]]*x) - (D0[[1]] + (D1[[1]] *exp(D2[[1]]*x))))
 }
 
-mod_predictions <- data.frame(x, growth = sapply(x, mod_pred))
+mod_predictions<- data.frame(x, growth = sapply(x, mod_pred))
 p <- ggplot(data = data.frame(x = 0), mapping = aes(x = x))
 p + geom_line(aes(x = x, y = growth), data = mod_predictions) +
-	ylim(0, 2) + xlab("Temperature (°C)") + ylab("Growth")
+	ylim(0, 1) + xlab("Temperature (°C)") + ylab("Growth rate")
 
 get_topt <- function(df){
 	grfunc<-function(x){
 		-mod_pred(x)
 	}
-	optinfo<-optim(c(x=df$z[[1]]),grfunc)
+	?optim
+	optinfo<-optim(c(20,grfunc))
 	opt <-c(optinfo$par[[1]])
 	maxgrowth <- c(-optinfo$value)
 	results <- data.frame(topt = opt, rmax = maxgrowth)
@@ -31,3 +58,8 @@ get_topt <- function(df){
 }
 
 get_topt
+
+data <- read_delim("data/all_species_IDE.txt", delim = ",")
+data %>% 
+	filter(Species == "AC", term == "b1") %>% 
+	select(estimate)
