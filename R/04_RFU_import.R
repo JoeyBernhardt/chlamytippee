7740ebf0c999207bda0a242db5794bf0b5d406bc
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-08-23 18:57:47 +0200

    adding data from Aug 23 pilot (day 2 of experiment)

---
+++ R/04_RFU_import.R
@@ -0,0 +1,93 @@
+
+
+library(tidyverse)
+library(readxl)
+
+
+plate1 <- read_excel("data/RFU-2018-08-23-2shakes/plate1.xlsx", skip = 47) %>% 
+	select(-X__2) %>% 
+	rename(row = X__1)
+write_csv(plate1, "data-processed/plate1-CT-pilot-day2.csv")
+plate2 <- read_excel("data/RFU-2018-08-23-2shakes/plate2.xlsx", skip = 47) %>% 
+	select(-X__2) %>% 
+	rename(row = X__1)
+write_csv(plate2, "data-processed/plate2-CT-pilot-day2.csv")
+
+plate3 <- read_excel("data/RFU-2018-08-23-2shakes/plate3.xlsx", skip = 47) %>% 
+	select(-X__2) %>% 
+	rename(row = X__1)
+write_csv(plate3, "data-processed/plate3-CT-pilot-day2.csv")
+
+plate4 <- read_excel("data/RFU-2018-08-23-2shakes/plate4.xlsx", skip = 47) %>% 
+	select(-X__2) %>% 
+	rename(row = X__1)
+write_csv(plate4, "data-processed/plate4-CT-pilot-day2.csv")
+
+
+plate5 <- read_excel("data/RFU-2018-08-23-2shakes/plate5.xlsx", skip = 47) %>% 
+	select(-X__2) %>% 
+	rename(row = X__1)
+write_csv(plate5, "data-processed/plate5-CT-pilot-day2.csv")
+
+
+
+# Bring em in -------------------------------------------------------------
+
+plate1df <- read_plate(file = "data-processed/plate1-CT-pilot-day2.csv", well_ids_column = "well") %>% 
+	mutate(plate = "plate1") %>% 
+	mutate(well = str_replace(well, "0", "")) %>% 
+	rename(RFU = row) %>% 
+	mutate(light = 50)
+
+plate2df <- read_plate(file = "data-processed/plate2-CT-pilot-day2.csv", well_ids_column = "well") %>% 
+	mutate(plate = "plate2") %>% 
+	mutate(well = str_replace(well, "0", "")) %>% 
+	rename(RFU = row) %>% 
+	mutate(light = 33)
+
+plate3df <- read_plate(file = "data-processed/plate3-CT-pilot-day2.csv", well_ids_column = "well") %>% 
+	mutate(plate = "plate3") %>% 
+	mutate(well = str_replace(well, "0", "")) %>% 
+	rename(RFU = row) %>% 
+	mutate(light = 70)
+
+plate4df <- read_plate(file = "data-processed/plate4-CT-pilot-day2.csv", well_ids_column = "well") %>% 
+	mutate(plate = "plate4") %>% 
+	mutate(well = str_replace(well, "0", "")) %>% 
+	rename(RFU = row) %>% 
+	mutate(light = 100)
+
+
+plate5df <- read_plate(file = "data-processed/plate5-CT-pilot-day2.csv", well_ids_column = "well") %>% 
+	mutate(plate = "plate5") %>% 
+	mutate(well = str_replace(well, "0", "")) %>% 
+	rename(RFU = row) %>% 
+	mutate(light = 5)
+
+
+all_RFUs <- bind_rows(plate1df, plate2df, plate3df, plate4df, plate5df)
+
+RFU_all <- left_join(all_RFUs, plate_pilot, by = "well")
+RFU_all2 <- left_join(RFU_all, dilutions, by = "population_density")
+
+RFU_all2 %>% 
+	ggplot(aes(x = percent_of_stock, y = RFU)) + geom_point() +
+	facet_wrap( ~ light)
+
+ggplot(data = RFU_all2, aes(x=percent_of_stock, y=light, fill=RFU)) + 
+	geom_tile(size = 4)
+
+
+RFU_files <- c(list.files("data/RFU-2018-08-23-2shakes", full.names = TRUE))
+
+
+
+names(RFU_files) <- RFU_files %>% 
+	gsub(pattern = ".xlsx$", replacement = "")
+
+all_plates <- map_df(RFU_files, read_excel, skip = 47, .id = "file_name") %>% 
+	select(-X__2) %>% 
+	rename(row = X__1)
+plate1 <- all_plates %>% 
+	filter(grepl("plate1", file_name)) %>% 
+	select(-file_name)
