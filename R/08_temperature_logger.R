f41fbff103b71af43778c50eee0a2a03f23e137e
Joey Bernhardt <joey.bernhardt@biodiversity.ubc.ca>
2018-09-18 13:37:51 +0200

    uploading data from loggers

--- R/08_temperature_logger.R
+++ R/08_temperature_logger.R
@@ -22,7 +22,8 @@ inc5 %>%
 	mutate(daytime = NA) %>% 
 	mutate(daytime = ifelse(hour > 9 & hour < 15.5, "night", "day")) %>% 
 	filter(date > ymd("2018-09-14")) %>% 
-	ggplot(aes(x = date_time, y = temperature, color = daytime)) + geom_point()
+	ggplot(aes(x = date_time, y = temperature, color = daytime)) + geom_point() 
+ggsave("figures/temperature_logger5.pdf", width = 5, height = 4)
 
 
 inc25 <- read_csv("data/temperature-loggers/logger30-25C-2018-09-13.csv", skip = 1) %>% 
@@ -58,7 +59,7 @@ inc20 %>%
 	filter(date > ymd("2018-09-06")) %>% 
 	filter(temperature < 22) %>% 
 	# mutate(date_time = as.date(date_time)) %>% 
-	ggplot(aes(x = hour, y = temperature, color = daytime)) + geom_point() +
-	facet_wrap( ~ month_day)
-	scale_x_date(date_minor_breaks = "1 hour", format = "%d - %H")
+	ggplot(aes(x = date_time, y = temperature, color = daytime)) + geom_point()
+ggsave("figures/temperature_logger20.pdf", width = 5, height = 4)
+	
 
